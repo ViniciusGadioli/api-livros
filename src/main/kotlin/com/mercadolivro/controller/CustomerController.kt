b91cc77a1f7d77a9145ba54b9795c9b26c016370
Vinicius Gadioli <vinicius.gadioli@cobli.co>
2025-09-18 00:06:52 -0300

    add: starting api

---
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -0,0 +1,27 @@
+package com.mercadolivro.controller
+
+import com.mercadolivro.controller.request.PostCustomerRequest
+import com.mercadolivro.model.CustomerModel
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("customer")
+class CustomerController {
+
+    @GetMapping
+    fun getCustomer(): CustomerModel{
+        return CustomerModel(id = "1", name = "Vinicius", email = "vini@gmail.com")
+    }
+
+    @PostMapping
+    fun createCustomer(@RequestBody customer: PostCustomerRequest){
+        println(customer)
+    }
+
+
+}
